6dcaaefcaac22fd3665fc036cc18b09564cb3b02
andylnx <andylnx@archlinux>
2025-09-27 17:24:55 -0500

    hola mundo

--- src/javaproject/JavaProject.java
+++ src/javaproject/JavaProject.java
@@ -5,6 +5,10 @@ public class JavaProject {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        int[] arr = {1,2,3,4,5,6};
+        for(int e : arr){
+            System.out.println("hola numero "+e);
+        }
     }
     
 }
